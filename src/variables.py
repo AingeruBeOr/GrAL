d9e7c288266910f4d6fa3b96c5115dc96dc68b81
Aingeru <1405bellido@gmail.com>
2024-02-21 12:14:06 +0100

    created variables.py file

--- src/variables.py
+++ src/variables.py
@@ -1,1 +1,11 @@
-PATH_TO_MEDMCQA_DATASET = 'data/'
+# Description: This file contains the paths to the datasets and the models.
+
+# Path to MedMCQA dataset
+PATH_TO_MEDMCQA_DATASET = '../data/MedMCQA/'
+PATH_TO_MEDMCQA_TRAIN = PATH_TO_MEDMCQA_DATASET + 'train.json'
+PATH_TO_MEDMCQA_DEV = PATH_TO_MEDMCQA_DATASET + 'dev.json'
+PATH_TO_MEDMCQA_TEST = PATH_TO_MEDMCQA_DATASET + 'test.json'
+
+
+# Path to EriBERTa model
+PATH_TO_ERIBERTA = '../models/eriberta_libre/'

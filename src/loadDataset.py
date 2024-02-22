efeb6fac2f753f9cf2dbfe940460f5bc89f404df
Aingeru <1405bellido@gmail.com>
2024-02-22 15:46:30 +0100

    Added function to choose btw with context / w/o context

--- src/loadDataset.py
+++ src/loadDataset.py
@@ -8,7 +8,7 @@ class MedMCQA_Dataset(Dataset):
         self.labels = labels
 
     def __len__(self):
-        return len(self.json_loaded)
+        return len(self.encodings)
 
     def __getitem__(self, index):
         item = {key: torch.tensor(val[index]) for key, val in self.encodings.items()}
@@ -16,14 +16,29 @@ class MedMCQA_Dataset(Dataset):
         return item
     
 
-def load_dataset_as_dict(path):
+def load_dataset_as_dict_with_context(path):
     with open(path, 'r') as file:
         dataset = [json.loads(row) for row in file]
 
     # Formato de dataset_dict:
     # {'inputs': ['<s>exp</s>question</s>opa</s>opb</s>opc</s>opd', ...], 'labels': [1, 2, 3, 4, ...]}
     dataset_dict = {}
-    dataset_dict['inputs'] = [f'<s>{instance["exp"]}</s>{instance["question"]}</s>{instance["opa"]}</s>{instance["opb"]}</s>{instance["opc"]}</s>{instance["opd"]}</s>' for instance in dataset]
+    # No hace falta poner los tokens de inicio y fin de secuencia porque el tokenizador ya los añade automáticamente
+    dataset_dict['inputs'] = [f'{instance["exp"]}</s>{instance["question"]}</s>{instance["opa"]}</s>{instance["opb"]}</s>{instance["opc"]}</s>{instance["opd"]}' for instance in dataset]
+    dataset_dict['labels'] = [instance["cop"] for instance in dataset]
+
+    return dataset_dict
+
+
+def load_dataset_as_dict_wo_context(path):
+    with open(path, 'r') as file:
+        dataset = [json.loads(row) for row in file]
+
+    # Formato de dataset_dict:
+    # {'inputs': ['<s>question</s>opa</s>opb</s>opc</s>opd', ...], 'labels': [1, 2, 3, 4, ...]}
+    dataset_dict = {}
+    # No hace falta poner los tokens de inicio y fin de secuencia porque el tokenizador ya los añade automáticamente
+    dataset_dict['inputs'] = [f'{instance["question"]}</s>{instance["opa"]}</s>{instance["opb"]}</s>{instance["opc"]}</s>{instance["opd"]}' for instance in dataset]
     dataset_dict['labels'] = [instance["cop"] for instance in dataset]
 
     return dataset_dict
--- src/train.py
+++ src/train.py
@@ -1,16 +1,38 @@
 from transformers import AutoTokenizer, AutoModelForSequenceClassification, Trainer, TrainingArguments
 from loadDataset import MedMCQA_Dataset, load_dataset_as_dict
-from variables import PATH_TO_MEDMCQA_TRAIN, PATH_TO_MEDMCQA_DEV, PATH_TO_ERIBERTA
+from variables import PATH_TO_MEDMCQA_TRAIN, PATH_TO_MEDMCQA_DEV, PATH_TO_ERIBERTA, PATH_TO_BASELINE_OUTPUT
 import torch
+import os
 
 # https://huggingface.co/docs/transformers/main/en/tasks/sequence_classification
 
 
+def delete_more_than_512_tokens(dataset):
+    """
+    Delete instances with more than 512 tokens
+    """
+    number_of_instances = 0
+    for instance in dataset['input_ids']:
+        if len(instance) > 512:
+            dataset['input_ids'].remove(instance)
+            number_of_instances += 1
+    return dataset, number_of_instances
+
+
 ## 0. SET UP ##
+# Set up hyperparameters
 LR = 2e-5
 EPOCHS = 1
 BATCH_SIZE = 16
+TYPE = 'wo_context' # set to 'with_context' or 'wo_context'
+
+# Set up Weights & Biases
+os.environ["WANDB_PROJECT"]="tfg-baseline" # set the wandb project where this run will be logged
+os.environ["WANDB_LOG_MODEL"] = "checkpoint" # save your trained model checkpoint to wandb
+
+# Set up device
 device = "cuda" if torch.cuda.is_available() else "cpu"
+print(f"Device: {device}")
 
 
 ## 1. LOAD DATASET ##
@@ -24,8 +46,17 @@ dev_dataset = load_dataset_as_dict(PATH_TO_MEDMCQA_DEV)
 tokenizer = AutoTokenizer.from_pretrained(PATH_TO_ERIBERTA, use_fast=True)
 
 # Tokenize and encode
-encoded_train = tokenizer(train_dataset['inputs']).to(device) # TODO: hace falta truncar?? Qué estrategia usamos??
-encoded_dev = tokenizer(dev_dataset['inputs']).to(device) # TODO: hace falta truncar?? Qué estrategia usamos??
+print("Tokenizing and encoding datasets...")
+encoded_train = tokenizer(train_dataset['inputs'], padding=True) # TODO: hace falta truncar?? Qué estrategia usamos??
+print("\tTrain dataset tokenized and encoded. Number of instances: ", len(encoded_train['input_ids']))
+encoded_dev = tokenizer(dev_dataset['inputs'], padding=True) # TODO: hace falta truncar?? Qué estrategia usamos??
+print("\tDev dataset tokenized and encoded. Number of instances: ", len(encoded_dev['input_ids']))
+
+# Get rid of instances with more than 512 tokens (TODO: probar otras estraegias)
+encoded_train, number_of_deleted_instances = delete_more_than_512_tokens(encoded_train)
+print("\tDeleted instances with more than 512 tokens from train dataset. Number of instances: ", len(encoded_train['input_ids']), "Deleted: ", number_of_deleted_instances)
+encoded_dev, number_of_deleted_instances = delete_more_than_512_tokens(encoded_dev)
+print("\tDeleted instances with more than 512 tokens from train dataset. Number of instances: ", len(encoded_dev['input_ids']), "Deleted: ", number_of_deleted_instances)
 
 
 ## 3. FINE-TUNE MODEL ##
@@ -38,7 +69,7 @@ model = AutoModelForSequenceClassification.from_pretrained(PATH_TO_ERIBERTA).to(
 
 # SET TRAINING HYPERPARAMETERS #
 training_args = TrainingArguments(
-    output_dir='',                              # output directory
+    output_dir=PATH_TO_BASELINE_OUTPUT,         # output directory
     evaluation_strategy='steps',                # when to evaluate
     num_train_epochs=EPOCHS,                    # total number of training epochs
     per_device_train_batch_size=BATCH_SIZE,     # batch size per device during training
@@ -47,8 +78,9 @@ training_args = TrainingArguments(
     #weight_decay=
     logging_dir='./logs',                       # directory for storing logs
     logging_steps=10,                           # when to print log (and to evaluate if evaluation_strategy = 'steps')
-    load_best_model_at_end=True                 # load or not best model at the end
+    load_best_model_at_end=True,                # load or not best model at the end
     #use_mps_device=True                        # para que use la GPU del MAC
+    report_to='wandb'                           # report to Weights & Biases
 )
 
 # SET TRAINER: MODEL TO TRAIN, TRAINING ARGUMENTS, DATSET SPLITS...
